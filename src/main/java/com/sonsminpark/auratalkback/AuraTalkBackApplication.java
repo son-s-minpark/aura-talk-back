f047482d58342c26e824078776ce32b732231c2b
seohee-P <p03061180@gmail.com>
2025-03-11 00:19:14 +0900

    init: Spring Boot Project setup

---
+++ src/main/java/com/sonsminpark/auratalkback/AuraTalkBackApplication.java
@@ -0,0 +1,13 @@
+package com.sonsminpark.auratalkback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraTalkBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraTalkBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonsminpark/auratalkback/AuraTalkBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonsminpark.auratalkback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraTalkBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

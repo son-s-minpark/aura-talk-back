fc48ed4d98460536ec3ad2338dcb444a726582bc
mango606 <yuj08184@kangwon.ac.kr>
2025-04-13 05:05:48 +0900

    feat: SecurityConfig에 관심사 API 추가

--- src/main/java/com/sonsminpark/auratalkback/global/config/SecurityConfig.java
+++ src/main/java/com/sonsminpark/auratalkback/global/config/SecurityConfig.java
@@ -40,6 +40,9 @@ public class SecurityConfig {
                                 // 인증 없이 접근 가능
                                 .requestMatchers("/api/users/login", "/api/users",
                                         "/api/users/verify-email", "/api/users/resend-verification").permitAll()
+                                // 관심사 API 접근 설정
+                                .requestMatchers("/api/interests", "/api/interests/category/**").permitAll()
+                                .requestMatchers("/api/interests/*/users").authenticated()
                                 // TODO: 인증 설정 필요하면 추가하기
                                 .anyRequest().authenticated())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, tokenBlacklistService),

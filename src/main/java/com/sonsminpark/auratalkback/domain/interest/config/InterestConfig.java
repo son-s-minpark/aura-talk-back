7db73251d99b8e0afd3aad1014b7a6d9a426a1f6
mango606 <yuj08184@kangwon.ac.kr>
2025-04-13 05:04:39 +0900

    feat: 관심사 서비스 구현

---
+++ src/main/java/com/sonsminpark/auratalkback/domain/interest/config/InterestConfig.java
@@ -0,0 +1,9 @@
+package com.sonsminpark.auratalkback.domain.interest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.sonsminpark.auratalkback.domain.interest")
+public class InterestConfig {
+}
